65549ba6019d40c5b75b60c8a28971cfe09aafe4
LightSlateGray <lightslategray@nurfuerspam.de>
2018-07-18 23:14:23 +0200

    Remove URL validation and update internal properties
    Remove the over-engineered validation of the supplied URL because the input field is cleared way too often which does not help at all due to the fact that no indication is shown why this has happened. Instead, allow any kind of valid URL and let the user click the 'Test' button on the UI if validation of arguments is desired. The setter of property
    'URL' does now set the values for properties 'ViewFarmReportUrl' and 'XmlStatusReportUrl' as well by stripping the query (if set) and the last segment of the supplied URL to build a valid URL.

--- src/AnyStatus.Plugins/Widgets/DevOps/CruiseControlNet/CruiseControlNetWidget.cs
+++ src/AnyStatus.Plugins/Widgets/DevOps/CruiseControlNet/CruiseControlNetWidget.cs
@@ -2,7 +2,6 @@ using AnyStatus.API;
 using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
 using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
 
 namespace AnyStatus.Plugins.Widgets.DevOps.CruiseControlNet
@@ -13,30 +12,28 @@ namespace AnyStatus.Plugins.Widgets.DevOps.CruiseControlNet
     [DisplayName("CruiseControl.NET project")]
     [Description("CruiseControl.NET projects status.")]
     [DisplayColumn("DevOps")]
-    public class CruiseControlNetWidget : Build, IHealthCheck, ISchedulable, IWebPage
+    public class CruiseControlNetWidget : Build, IHealthCheck, ISchedulable, IStartable, IWebPage
     {
+        /// <summary>
+        /// Constant value referring to the web dashboard of the CruiseControl.NET farm overview.
+        /// </summary>
         private const string VIEW_FARM_REPORT = "ViewFarmReport.aspx";
+
+        /// <summary>
+        /// Constant value referring to the web page where the CruiseControl.NET Server publishes the current status
+        /// of all integration projects as XML.
+        /// </summary>
         private const string XML_STATUS_REPORT = "XmlStatusReport.aspx";
-        private const string CCNET_SEGMENT = "ccnet";
 
         /// <summary>
-        /// Indicates the valid suffixes for an absolute URL to the CruiseControl.NET server.
+        /// Stores the URL supplied by the user which is used to build the final URL to either open the web browser
+        /// or request the project status as XML.
         /// </summary>
-        /// <remarks>
-        /// Matches the end (last segment) of the following four URIs:
-        ///   - http://CC_SERVER_NAME/ccnet/ViewFarmReport.aspx
-        ///   - http://CC_SERVER_NAME/ccnet/XmlStatusReport.aspx
-        ///   - http://CC_SERVER_NAME/ccnet/
-        ///   - http://CC_SERVER_NAME/ccnet
-        /// </remarks>
-        private static readonly Regex ValidCruiseControlUrlPathSuffix =
-            new Regex(@"(?:ViewFarmReport\.aspx|XmlStatusReport\.aspx|ccnet\/?)$",
-                RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+        private string _url;
 
         /// <summary>
-        /// Stores the base URL used to build the final URL to either open the web browser or request the project status.
+        /// Stores the name of the project for which the status is requested.
         /// </summary>
-        private string _baseUrl;
         private string _projectName;
 
         /// <summary>
@@ -51,8 +48,24 @@ namespace AnyStatus.Plugins.Widgets.DevOps.CruiseControlNet
             "Usually, this refers to http://CC_SERVER_NAME/ccnet/ViewFarmReport.aspx or something similar.")]
         public string URL
         {
-            get { return this.ViewFarmReportUrl; }
-            set { _baseUrl = this.ValidateUrl(value); }
+            get { return _url; }
+            set
+            {
+                _url = value;
+
+                // Get base URL from the supplied value and update the URls used internally
+                var baseUrl = GetBaseUrl(value);
+                if (string.IsNullOrWhiteSpace(baseUrl))
+                {
+                    this.ViewFarmReportUrl = string.Empty;
+                    this.XmlStatusReportUrl = string.Empty;
+                }
+                else
+                {
+                    this.ViewFarmReportUrl = baseUrl + VIEW_FARM_REPORT;
+                    this.XmlStatusReportUrl = baseUrl + XML_STATUS_REPORT;
+                }
+            }
         }
 
         /// <summary>
@@ -72,42 +85,31 @@ namespace AnyStatus.Plugins.Widgets.DevOps.CruiseControlNet
         /// The URL of the CruiseControl.NET server web dashboard, also known as farm report.
         /// </summary>
         [Browsable(false)]
-        public string ViewFarmReportUrl
-          => string.IsNullOrWhiteSpace(this._baseUrl) ? string.Empty : _baseUrl + VIEW_FARM_REPORT;
+        public string ViewFarmReportUrl { get; private set; }
 
         /// <summary>
-        /// The URL where the CruiseControl.NET server published the XML status report of all projects.
+        /// The URL where the CruiseControl.NET server publishes the XML status report of all projects.
         /// </summary>
         [Browsable(false)]
-        public string XmlReportUrl
-          => string.IsNullOrWhiteSpace(this._baseUrl) ? string.Empty : _baseUrl + XML_STATUS_REPORT;
+        public string XmlStatusReportUrl { get; private set; }
 
         /// <summary>
-        /// Verifies that the supplied <paramref name="url"/> is valid.
+        /// Retrieves the base URL from the supplied <paramref name="url"/> which is used to construct the URL of the
+        /// web dashboard (<see cref="ViewFarmReportUrl"/>) and the URL from which the XML response containing the status
+        /// of all integration projects will be retrieved (<see cref="XmlStatusReportUrl"/>).
         /// </summary>
         /// <param name="url">The URL to verify if it points to a CruiseControl.Net server.</param>
         /// <returns></returns>
         /// <remarks>
-        /// Valid URLs are http://CC_SERVER_NAME/ccnet/XmlStatusReport.aspx and http://CC_SERVER_NAME/ccnet/ViewFarmReport.aspx for example.
+        /// Valid URLs include http://CC_SERVER_NAME/ccnet/XmlStatusReport.aspx, http://CC_SERVER_NAME/ccnet/ViewFarmReport.aspx
+        /// and http://CC_SERVER_NAME/ccnet/local/ViewStatusReport.aspx for example.
         /// </remarks>
-        private string ValidateUrl(string url)
+        private static string GetBaseUrl(string url)
         {
-            // Return an empty string if no useful string is supplied
-            if (string.IsNullOrWhiteSpace(url))
-            {
-                return string.Empty;
-            }
-
             try
             {
-                // Parse the supplied string a valid URL or throw an exception if it is invalid
+                // Parse the supplied string as valid URL or throw an exception if it is invalid
                 var uri = new Uri(url, UriKind.Absolute);
-
-                // Validate scheme is supported (i.e. http or https) 
-                if (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps)
-                {
-                    return string.Empty;
-                }
 
                 // Remove query string from URL if present
                 if (!string.IsNullOrWhiteSpace(uri.Query))
@@ -115,37 +117,15 @@ namespace AnyStatus.Plugins.Widgets.DevOps.CruiseControlNet
                     uri = new Uri(uri.AbsoluteUri.Replace(uri.Query, string.Empty), UriKind.Absolute);
                 }
 
-                if (!ValidCruiseControlUrlPathSuffix.IsMatch(uri.AbsoluteUri))
+                // Check if the URL ends with a slash
+                if (uri.AbsoluteUri.EndsWith("/"))
                 {
-                    return string.Empty;
+                    return uri.AbsoluteUri;
                 }
-
-                // Verify that the supplied URL consists of at least a single segment
-                var segments = uri.Segments; // -> e.g. ["/", "ccnet/", "XmlStatusReport.aspx"]
-                if (segments == null || segments.Length <= 0)
+                else
                 {
-                    return string.Empty;
+                    return uri.AbsoluteUri.Substring(0, uri.AbsoluteUri.LastIndexOf('/') + 1);
                 }
-
-                // Check the last segment of the supplied URL
-                var lastSegment = segments[segments.Length - 1];
-                if (CCNET_SEGMENT.Equals(lastSegment, StringComparison.OrdinalIgnoreCase)
-                    || $"{CCNET_SEGMENT}/".Equals(lastSegment, StringComparison.OrdinalIgnoreCase))
-                {
-                    // Return the base URL (e.g. http://CC_NET_SERVER/ccnet/) which will be used to open the browser and the web dashboard
-                    return uri.AbsoluteUri.EndsWith("/") ? uri.AbsoluteUri : uri.AbsoluteUri + "/";
-                }
-
-                // Validate that the path either ends with web dashboard page 'ViewFarmReport.aspx' or XML report page 'XmlStatusReport.apsx'
-                if (XML_STATUS_REPORT.Equals(lastSegment, StringComparison.OrdinalIgnoreCase)
-                    || VIEW_FARM_REPORT.Equals(lastSegment, StringComparison.OrdinalIgnoreCase))
-                {
-                    // Return the base URL (e.g. http://CC_NET_SERVER/ccnet/) which will be used to open the browser and the web dashboard
-                    var baseUrl = uri.AbsoluteUri.Replace(lastSegment, string.Empty);
-                    return baseUrl.EndsWith("/") ? baseUrl : baseUrl + "/";
-                }
-
-                return string.Empty;
             }
             catch (UriFormatException)
             {
